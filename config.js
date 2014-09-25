2229d0441d2c4997d8ab41f478443b970eda43f0
dannyg11 <daniel.a.gigante11@gmail.com>
2014-09-24 20:32:34 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {};
  
 config.twilio = {};
-config.twilio.key = 'a7d32d0b096dcb8cb08df2e1f4866558';
+config.twilio.key = '';
 config.twilio.disableSigCheck = false;
  
 module.exports = config;
